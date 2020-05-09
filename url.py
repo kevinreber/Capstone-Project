e86d5d564c09ca49eaa0199bf0735382398fe079
kevinreber <kevinreber1@gmail.com>
2020-05-09 14:34:58 -0700

    path to user's download folder(MAC OS)

--- url.py
+++ url.py
@@ -1,2 +1,5 @@
+import os
+
 BASE_URL = 'https://api.everypixel.com/v1/keywords'
 IMG_URL = '/Users/kevinreber/Documents/Code/00-Projects/00-Capstone Project 1/static/uploads'
+DOWNLOAD_FOLDER = os.path.expanduser("~")+"/Downloads/"

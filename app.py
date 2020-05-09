93f8583ef0fec0f577ff7f5eaa10c9a457a76fb6
kevinreber <kevinreber1@gmail.com>
2020-05-08 18:47:29 -0700

    added manual validators to check if file uploaded is valid image

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 import os
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from forms import ShutterStockForm, ImageUploadForm
 import requests
 from secrets import CLIENT_ID, CLIENT_SECRET, BASE_URL
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
 app.config['IMAGE_UPLOADS'] = '/Users/kevinreber/Documents/Code/00-Projects/00-Capstone Project 1/static/uploads'
+app.config['ALLOWED_IMAGE_EXTENSIONS'] = ["PNG", "JPG", "JPEG"]
+
+IMG_URL = '/Users/kevinreber/Documents/Code/00-Projects/00-Capstone Project 1/static/uploads'
+
+
+def allowed_image(filename):
+    """Checks if file uploaded is a valid image"""
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config['ALLOWED_IMAGE_EXTENSIONS']:
+        return True
+    else:
+        return False
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -17,53 +35,58 @@ def home():
     form = ImageUploadForm()
 
     if form.validate_on_submit():
-        form = ShutterStockForm()
 
         if request.files:
 
             # get image from form
             image = request.files["image"]
 
-            # save image to 'upload' folder
-            image.save(os.path.join(
-                app.config['IMAGE_UPLOADS'], image.filename))
+            if image.filename == "":
+                flash("Image must have a filename", "danger")
+                return redirect("/")
 
-            # path to image
-            image_path = f"/Users/kevinreber/Documents/Code/00-Projects/00-Capstone Project 1/static/uploads/{image.filename}"
+            if not allowed_image(image.filename):
+                flash("File must be JPG or PNG", "danger")
+                return redirect("/")
 
-            # TODO: Make num_of_keywords dynamic, default to 5 for now
-            num_of_keywords = 5
+            else:
+                filename = secure_filename(image.filename)
 
-            params = {"num_keywords": num_of_keywords}
-
-            # open image and send request to API
-            with open(image_path, "rb") as request_image:
-                data = {"data": request_image}
-
-                resp = requests.post(BASE_URL,
-                                     files=data, params=params, auth=(CLIENT_ID, CLIENT_SECRET)).json()
-
-            keywords = [key["keyword"] for key in resp["keywords"]]
-
-            print("#######################################")
-            print("#######################################")
-            print(keywords)
-            print(f"{image} saved")
-            print("#######################################")
-
-            return render_template("prepare_export.html", keywords=keywords, form=form)
+                # save image to 'upload' folder
+                image.save(os.path.join(
+                    app.config['IMAGE_UPLOADS'], filename))
+                print("Image saved")
+                return redirect(f"/img/{filename}")
 
     else:
         return render_template("image_upload.html", form=form)
 
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     form = ImageUploadForm()
+@app.route(f'/img/<image_name>', methods=['GET', 'POST'])
+def file_data(image_name):
 
-#     if form.validate_on_submit():
+    form = ShutterStockForm()
 
-#         filename = images.save(form.image.data)
-#         return f'Filename: { filename }'
+    # TODO: Make num_of_keywords dynamic, default to 5 for now
+    num_of_keywords = 3
 
-#     return render_template('index.html', form=form)
+    params = {"num_keywords": num_of_keywords}
+
+    # path to image
+    image_path = f"{IMG_URL}/{image_name}"
+
+    # open image and send request to API
+    with open(image_path, "rb") as image:
+
+        data = {"data": image}
+        resp = requests.post(BASE_URL, files=data, params=params, auth=(
+            CLIENT_ID, CLIENT_SECRET)).json()
+
+    keywords = [key["keyword"] for key in resp["keywords"]]
+
+    # if form.validate_on_submit():
+
+    #     filename = images.save(form.image.data)
+    #     return f'Filename: { filename }'
+
+    return render_template("prepare_export.html", keywords=keywords, form=form)

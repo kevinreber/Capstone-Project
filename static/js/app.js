ef44e48c0b6c088b3c0847c84ae7c0522f6ae722
kevinreber <kevinreber1@gmail.com>
2020-05-11 00:41:06 -0700

    added tagify js to handle keyword tags

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+// Use Tagify for keywords UI
+// const keywords = new Tagify(document.querySelector("input[name=hero"));
+
+const keywords = document.querySelector("input[name=keywords");
+
+const keywordsTagify = new Tagify(keywords);

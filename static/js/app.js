90ab6c5423a5ab20886b8df0b020c116caf07500
kevinreber <kevinreber1@gmail.com>
2020-05-11 15:10:54 -0700

    tweaked styling

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,4 @@
 // Use Tagify for keywords UI
-// const keywords = new Tagify(document.querySelector("input[name=hero"));
-
 const keywords = document.querySelector("input[name=keywords");
 
 const keywordsTagify = new Tagify(keywords, {

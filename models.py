fffa879ccee8d60dbdce94000c8e58bedbea3054
kevinreber <kevinreber1@gmail.com>
2020-05-12 22:10:29 -0700

    added bcrypt and fixed db image.user_id relationship

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
@@ -13,13 +15,52 @@ def connect_db(app):
 
 class User(db.Model):
     """Users Model"""
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False, unique=True)
 
-    images = db.relationship('Image', backref="user",
-                             cascade="all, delete-orphan")
+    __tablename = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, nullable=False, unique=True)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.datetime.now)
+    images = db.relationship('Image', backref="user",
+                             cascade="all, delete")
+
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+        """Sign up user.
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
 
     @property
     def formated_date(self):
@@ -37,8 +78,8 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String, nullable=False, unique=True)
     source = db.Column(db.String, nullable=False, unique=True)
-    tags = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    keywords = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.datetime.now)
 
@@ -48,3 +89,4 @@ class Image(db.Model):
         """ex. Fri Apr 17 2020, 05:01 PM"""
 
         return self.created_at.strftime("%a %b %d %Y, %I:%M %p")
+

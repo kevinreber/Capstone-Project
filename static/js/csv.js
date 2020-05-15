17108af1d2b24a4fb3f1e9653ccfad231cd6a574
kevinreber <kevinreber1@gmail.com>
2020-05-15 11:26:17 -0700

    script: re-organized

--- static/js/csv.js
+++ static/js/csv.js
@@ -1,28 +1,3 @@
-// $(async function () {
-const API_URL = 'http://localhost:5000/api'
-
-// Use Tagify to create keywords UI
-let allKeywordsInput = document.querySelectorAll("input[name=keywords]");
-let allKeywordsTagify;
-
-$(document).ready(applyTagify(allKeywordsInput));
-
-function applyTagify(keywords) {
-
-    for (let keyword of keywords) {
-        allKeywordsTagify = new Tagify(keyword, {
-            dropdown: {
-                position: "input"
-            }
-        });
-    }
-}
-
-// ! TODO: fix "remove all tags"
-// "remove all tags" button event listener
-// document.querySelectorAll('.tags--removeAllBtn')
-//     .addEventListener('click', keywordsTagify.removeAllTags.bind(keywordsTagify));
-
 // ! TODO: handle multiple images
 // ? GET ALL LI ELEMENTS
 // ? SEND EVERYTHING AS JSON TO SERVER
@@ -93,21 +68,4 @@ function getKeywords(tags) {
     }
 
     return keywords.join();
-}
-
-// ! TODO remove <hr> tag when image is deleted
-// Handle Delete Image Button
-$("#image-list").on("click", ".delete-button", deleteImage);
-async function deleteImage(e) {
-    // Send delete request to API and remove li
-    e.preventDefault();
-
-    const imageLI = (e.target).closest("li");
-    const imageId = imageLI.id;
-    console.log(imageId);
-
-    await axios.delete(`${API_URL}/delete/${imageId}`)
-        .then(resp => console.log(resp))
-        .catch(err => console.log(err))
-    imageLI.remove();
 }
---
+++ static/js/ui.js
@@ -0,0 +1,41 @@
+// $(async function () {
+const API_URL = 'http://localhost:5000/api'
+
+// Use Tagify to create keywords UI
+let allKeywordsInput = document.querySelectorAll("input[name=keywords]");
+let allKeywordsTagify;
+
+$(document).ready(applyTagify(allKeywordsInput));
+
+function applyTagify(keywords) {
+
+    for (let keyword of keywords) {
+        allKeywordsTagify = new Tagify(keyword, {
+            dropdown: {
+                position: "input"
+            }
+        });
+    }
+}
+
+// ! TODO: fix "remove all tags"
+// "remove all tags" button event listener
+// document.querySelectorAll('.tags--removeAllBtn')
+//     .addEventListener('click', keywordsTagify.removeAllTags.bind(keywordsTagify));
+
+// ! TODO remove <hr> tag when image is deleted
+// Handle Delete Image Button
+$("#image-list").on("click", ".delete-button", deleteImage);
+async function deleteImage(e) {
+    // Send delete request to API and remove li
+    e.preventDefault();
+
+    const imageLI = (e.target).closest("li");
+    const imageId = imageLI.id;
+    console.log(imageId);
+
+    await axios.delete(`${API_URL}/delete/${imageId}`)
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+    imageLI.remove();
+}

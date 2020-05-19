79a9135650eb255473326ea66ff84289149ca7db
kevinreber <kevinreber1@gmail.com>
2020-05-19 09:58:47 -0700

    config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-class Config:
+class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # DEBUG TOOLBAR
@@ -10,7 +12,6 @@ class Config:
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
@@ -20,3 +21,21 @@ class Config:
     BASE_URL = os.environ.get('BASE_URL')
     IMG_URL = os.environ.get('IMG_URL')
     DOWNLOAD_FOLDER = os.path.expanduser("~")+"/Downloads/"
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    DEBUG_TB_ENABLED = True
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration."""
+    TESTING = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+    DEBUG_TB_ENABLED = False
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration."""
+    pass

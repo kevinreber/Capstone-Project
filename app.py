3d4d8086b7cff0aa14f311634bee13fd94a7579d
kevinreber <kevinreber1@gmail.com>
2020-06-21 17:50:09 -0700

    refacted: removed leftover 'api' code

--- app.py
+++ app.py
@@ -248,26 +248,10 @@ def user_profile():
 
     return render_template("info.html", form=form)
 
-
-# @app.route('/api/users/delete', methods=["DELETE"])
-# def delete_user():
-#     """Delete user."""
-#     if not g.user:
-#         flash("Access unauthorized.", "danger")
-#         return redirect(url_for("home"))
-
-#     do_logout()
-
-#     db.session.delete(g.user)
-#     db.session.commit()
-
-#     flash("User deleted", "success")
-#     return jsonify(message="User deleted")
-
-
 ##################################################################
 #   IMAGE ROUTES   ----------------------------------------------#
 ##################################################################
+
 
 @app.route("/images", methods=["GET"])
 def get_images():
@@ -344,126 +328,6 @@ def get_keywords(img_path, max_keywords):
     keywords = [key["keyword"] for key in resp["keywords"]]
 
     return keywords
-
-##################################################################
-#   API ROUTES   ------------------------------------------------#
-##################################################################
-
-# @app.route("/api/delete/all", methods=["DELETE"])
-# def delete_all_files():
-#     """Delete all images from DB and ImageKit.io"""
-
-#     if not g.user:
-#         images = Image.query.filter(Image.user_id == None).all()
-#         # Delete all from DB
-#         Image.query.filter(Image.user_id == None).delete()
-
-#     else:
-#         # Get image in DB via file_id
-#         images = Image.query.filter(Image.user_id == g.user.id).all()
-#         # Delete all from DB
-#         Image.query.filter(Image.user_id == g.user.id).delete()
-
-#     # Delete all images from ImageKit.io
-#     delete = [imagekit.delete_file(img.id) for img in images]
-
-#     # # Delete all from DB
-#     # Image.query.delete()
-#     db.session.commit()
-
-#     flash("Removed all images", "success")
-#     print("Removed all images", delete)
-#     return jsonify(message="Deleted all images")
-
-
-# @app.route("/api/delete/<file_id>", methods=["DELETE"])
-# def delete_file(file_id):
-#     """Delete from from DB and ImageKit.io"""
-
-#     # Get image in DB via file_id
-#     file = Image.query.get_or_404(file_id)
-
-#     # If a logged in user attempts to access another user's files
-#     # the logged in user will be redirected
-#     if g.user:
-#         if file.user_id != g.user.id:
-#             flash("Access unauthorized.", "danger")
-#             return redirect("/")
-
-#     # Delete from ImageKit.io
-#     delete = imagekit.delete_file(file_id)
-
-#     # Delete from DB
-#     db.session.delete(file)
-#     db.session.commit()
-
-#     flash("File Deleted", "success")
-#     print("Delete File-", delete)
-#     return jsonify(message="Deleted")
-
-
-# @app.route("/api/update", methods=["PATCH"])
-# def update_db():
-#     """Updates changes to DB"""
-
-#     if not g.user:
-#         flash("Access unauthorized.", "danger")
-#         return redirect("/")
-
-#     # Store json data passed in
-#     data = json.loads(request.json['jsonData'])
-#     file_ids = [file_id for file_id in data]
-
-#     # Add each image update to db.session
-#     for file_id in file_ids:
-#         update_file(file_id, data[file_id])
-
-#     # Serialize data and return JSON
-#     s_data = [serialize_data(data, file) for file in file_ids]
-#     img_json = jsonify(data=s_data)
-
-#     # Commit all changes made to DB
-#     db.session.commit()
-#     flash("Files Saved", "success")
-#     return (img_json, 201)
-
-##################################################################
-#   API HELPER FUNCTIONS   --------------------------------------#
-##################################################################
-
-# def update_file(file_id, data):
-#     """Updates file data in DB"""
-
-#     img = Image.query.get_or_404(file_id)
-
-#     img.filename = data.get("filename", img.filename)
-#     img.description = data.get("description", img.description)
-#     img.keywords = data.get("keywords", img.keywords)
-#     img.category1 = data.get("category1", img.category1)
-#     img.category2 = data.get("category2", img.category2)
-#     img.location = data.get("location", img.location)
-#     img.editorial = data.get("editorial", img.editorial)
-#     img.r_rated = data.get("r_rated", img.r_rated)
-
-#     db.session.add(img)
-#     print(f"Updated {img.filename}")
-
-
-# def serialize_data(data, id):
-#     """Serializes JSON data"""
-
-#     return {
-#         id: {
-#             "filename": data[id]["filename"],
-#             "description": data[id]["description"],
-#             "keywords": data[id]["keywords"],
-#             "category1": data[id]["category1"],
-#             "category2": data[id]["category2"],
-#             "editorial": data[id]["editorial"],
-#             "r_rated": data[id]["r_rated"],
-#             "location": data[id]["location"]
-#         }
-#     }
 
 
 if __name__ == '__main__':
